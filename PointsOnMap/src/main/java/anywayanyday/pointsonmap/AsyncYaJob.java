c5a0e964e03a5326502d9a5f0ff4d3abc0b4215b
Ivied <cer87@list.ru>
2013-10-06 17:16:33 +0400

    added parse yandex point api

--- PointsOnMap/src/main/java/anywayanyday/pointsonmap/AsyncYaJob.java
+++ PointsOnMap/src/main/java/anywayanyday/pointsonmap/AsyncYaJob.java
@@ -7,10 +7,14 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 
 public class AsyncYaJob {
 
@@ -20,7 +24,6 @@ public class AsyncYaJob {
         public void onYaResponse(String response);
     }
 
-
     public AsyncYaJob(String address, YaListener yaListener) {
         this.yaListener = yaListener;
         AsyncPointRequest pointRequest = new AsyncPointRequest();
@@ -29,37 +32,11 @@ public class AsyncYaJob {
 
     private class AsyncPointRequest extends AsyncTask<String, Void, String> {
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(String... params)  {
             String address = params[0];
             address = address.replace(" ", "+");
-            HttpGet request = new HttpGet("http://geocode-maps.yandex.ru/1.x/?geocode=" + address );
-            BufferedReader in;
-            String data = null;
-
-            try {
-                HttpClient client = new DefaultHttpClient();
-                HttpResponse response = client.execute(request);
-                response.getStatusLine().getStatusCode();
-
-                in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-                StringBuilder sb = new StringBuilder("");
-                String l;
-                String nl = System.getProperty("line.separator");
-                while ((l = in.readLine()) != null) {
-                    sb.append(l).append(nl);
-                }
-                in.close();
-                data = sb.toString();
-
-
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e ){
-                e.printStackTrace();
-            }
-            return data;
+            HttpGet request = new HttpGet("http://geocode-maps.yandex.ru/1.x/?results=1&geocode=" + address );
+            return getGeoData(httpRequest(request));
         }
 
         @Override
@@ -68,4 +45,60 @@ public class AsyncYaJob {
             yaListener.onYaResponse(results);
         }
     }
+
+    static private String httpRequest(HttpGet request) {
+        String data = null;
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response = client.execute(request);
+            response.getStatusLine().getStatusCode();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuilder sb = new StringBuilder("");
+            String line;
+            while ((line = in.readLine()) != null) {
+                sb.append(line);
+            }
+            in.close();
+            client.getConnectionManager().shutdown();
+            data = sb.toString();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    static private String getGeoData(String data) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            factory.setNamespaceAware(true);
+            parser.setInput(new StringReader(data));
+            return parseForGeoData(parser);
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    static private String parseForGeoData(XmlPullParser parser) throws XmlPullParserException, IOException {
+        int eventType = parser.getEventType();
+        while(eventType != XmlPullParser.END_DOCUMENT){
+            if(eventType == XmlPullParser.START_TAG){
+                String name = parser.getName();
+                if ( name.equalsIgnoreCase("pos"))  {
+                    parser.next();
+                    return parser.getText();
+                }
+            }
+            eventType = parser.next();
+        }
+        return null;
+    }
+
 }
